26b39b222e1c5d6aea4a3f20dc9f0334c935844e
trainv3 <nguyenvan.trai@hotmail.com>
2025-09-26 08:57:39 +0700

    Init2

--- Tong.c
+++ Tong.c
@@ -8,8 +8,8 @@ scanf("%d",&a);
 printf("Nhap so b=");
 scanf("%d",&b);
  Tong=a+b;
-
+Hieu =a-b;
 printf("Tong a+b =%d",Tong);
-//printf("\n Hieu a-b=%d",Hieu);
+printf("\n Hieu a-b=%d",Hieu);
 return 0;
 }
